a5dc102a0ab838afbc589c421b0737f72bc2007a
mohammed el houssni <91427616+elhoussnimed@users.noreply.github.com>
2022-10-26 10:06:12 +0100

    -mmodifie the portfolio section by adding a carrousel and make the year in the footer dynamic all that with javascript

---
+++ app.js
@@ -0,0 +1,52 @@
+const prevBtn = document.querySelector(
+  "section.portfolio .container .work .prev"
+);
+const nextBtn = document.querySelector(
+  "section.portfolio .container .work .next"
+);
+const portfolioItems = document.querySelectorAll(
+  "section.portfolio .container .work .parent"
+);
+
+let currentActive = 0;
+
+nextBtn.addEventListener("click", nextItem);
+prevBtn.addEventListener("click", prevItem);
+
+function nextItem() {
+  currentActive++;
+  portfolioItems[currentActive].classList.add("active");
+  update();
+}
+
+function prevItem() {
+  portfolioItems[currentActive].classList.remove("active");
+  currentActive--;
+  update();
+}
+
+function update() {
+  // update currentActive value
+  currentActive = currentActive;
+
+  // able or disable prevBtn if :
+  if (currentActive > 0) {
+    prevBtn.classList.remove("disable");
+  } else if (currentActive <= 0) {
+    prevBtn.classList.add("disable");
+  }
+
+  // able or disable nextBtn if :
+  if (currentActive === portfolioItems.length - 1) {
+    nextBtn.classList.add("disable");
+  } else {
+    nextBtn.classList.remove("disable");
+  }
+}
+
+// update the website year dynamic
+const websiteYear = document.querySelector(
+  "footer .container .right-footer .website-year"
+);
+const dynamicYear = new Date().getFullYear();
+websiteYear.innerHTML = dynamicYear;
